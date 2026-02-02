d7821ed013f9df5fde1482fc4d959b41cc0787bb
raibatroyc-prog <raibat.royc@gmail.com>
2026-02-02 08:46:10 +0530

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,7 @@ export const getAsset = (path: string) => {
 
 const siteConfig = {
   personal: {
-    name: 'Raibat Roy Choudhury',
+    name: 'Raibat Choudhury',
     title: 'Your Title | Your Role',
     image: getAsset('images/profile.png'), // Customize or replace with your profile image
     description:
